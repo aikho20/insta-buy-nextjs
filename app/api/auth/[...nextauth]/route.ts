1598661c18dcec4da34574167afef884cec618bf
marco.deguzman.ico <marco.deguzman.ico@gmail.com>
2024-04-19 08:39:35 +0000

    google login

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,16 +1,7 @@
 
-import NextAuth, { AuthOptions } from "next-auth";
-import GoogleProvider from 'next-auth/providers/google'
+import { authOptions } from "@/lib/authOptions";
+import NextAuth from "next-auth";
 
-
-export const authOptions: AuthOptions = {
-    providers: [
-        GoogleProvider({
-            clientId: process.env.GOOGLE_CLIENT_ID!,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET!
-        })
-    ]
-}
 const handler = NextAuth(authOptions)
 
 export { handler as GET, handler as POST }
