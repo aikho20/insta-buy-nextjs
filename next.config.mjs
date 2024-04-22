c598d4cfff27e98179eebda47d999aca9166a813
marco.deguzman.ico <marco.deguzman.ico@gmail.com>
2024-04-22 10:31:38 +0000

    registration

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,16 @@
 /** @type {import('next').NextConfig} */
+
+
+const { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET, NEXTAUTH_SECRET, MONGODB_URI, NEXTAUTH_URL } = process.env
+
+
 const nextConfig = {
     experimental: {
         serverActions: true
-    }
+    },
+    env: {
+        GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET, NEXTAUTH_SECRET, MONGODB_URI, NEXTAUTH_URL
+    },
 
 };
 
